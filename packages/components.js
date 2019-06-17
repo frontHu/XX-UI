f915d71684a684746ac1455a6e928e9cc2e60e31
huzhiwei <405163706@qq.com>
2019-06-17 13:21:24 +0800

    feat(page): ":pencil: docs"

--- packages/components.js
+++ packages/components.js
@@ -2,9 +2,9 @@
  * 导入的组件列表
  */
 // import Layout from './components/layout'; 
-import Button from './components/button';
+import Button from './components/button';  
 
 export default [
   // Layout,
-  Button
+  Button,
 ];
