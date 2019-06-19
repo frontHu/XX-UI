07b97060f653a9a40e42352f94e4af8f0fbda0cf
huzhiwei <405163706@qq.com>
2019-06-19 22:56:54 +0800

    docs: vuepress 搭建

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,6 +2,7 @@
  * 应用程序级别增强
  */
 import Vue from 'vue';
+import XXUI from './../../packages/loacl'
 
 export default ({
   Vue, // the version of Vue being used in the VuePress app
@@ -9,4 +10,5 @@ export default ({
   router // the router instance for the app
 }) => {
   // ...apply enhancements to the app
+  Vue.use(XXUI)
 }
